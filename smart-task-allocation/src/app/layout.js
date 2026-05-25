1ed19006e6f61dfa2946e039d49a866b6b25df8c
Paing Thit Xan <paingthitxan184@gmail.com>
2026-05-26 03:58:50 +0800

    metadata changes

--- smart-task-allocation/src/app/layout.js
+++ smart-task-allocation/src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Smart Task Allocation",
+  title: "Workflow+",
   description: "Smart task allocation application",
 };
 
