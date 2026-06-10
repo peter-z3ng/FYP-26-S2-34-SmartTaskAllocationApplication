1ac8921fc6d06038c34fc6422d3cb81f1bbe1ae2
yjy2046-cloud <yjy2046@gmail.com>
2026-06-10 20:43:26 +0800

    Delete smart-task-allocation directory

--- smart-task-allocation/database/20260528_add_profile_picture_url.sql
+++
@@ -1,2 +0,0 @@
-alter table if exists public.profile
-add column if not exists profile_picture_url text;
--- smart-task-allocation/database/20260528_add_subscription_tier.sql
+++
@@ -1,6 +0,0 @@
-alter table if exists public.user_account
-  add column if not exists subscription_tier varchar not null default 'Free';
-
-update public.user_account
-set subscription_tier = 'Free'
-where subscription_tier is null;
